ab98e54bf4aabca0ef9fc76f5b1fb7c666207e7b
siska199 <siska.apriana.1703226@students.um.ac.id>
2026-01-08 10:42:57 +0700

    update: url be

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 export const API = axios.create({
-    baseURL :  "https://dewetour-be.vercel.app/api199/v1/"
+    baseURL :  "https://dewetour-be-199.vercel.app/api199/v1/"
 })
+
+
 // "https://dewetour-backend.herokuapp.com/api199/v1/" ||
 
 export const setAuthToken = (token) =>{
